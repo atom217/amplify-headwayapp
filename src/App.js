2038975a087408b7c900b73ef27337810b3e284c
pushpendra <pp@wearemobedia.com>
2019-08-25 21:54:08 +0530

    added code for headwayapp

--- src/App.js
+++ src/App.js
@@ -2,33 +2,34 @@ import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const HW_config = {
-  selector: ".App-header", // CSS selector where to inject the badge
-  account:  "xGR4zy",
-  callbacks: {
-    onWidgetReady: function(widget) {
-      console.log("Widget is here!");
-      console.log("unseen entries count: " + widget.getUnseenCount());
-    },
-    onShowWidget: function(){
-      console.log("Someone opened the widget!");
-    },
-    onShowDetails: function(changelog){
-      console.log(changelog.position); // position in the widget
-      console.log(changelog.id); // unique id
-      console.log(changelog.title); // title
-      console.log(changelog.category); // category, lowercased
-    },
-    onReadMore: function(changelog){
-      console.log(changelog); // same changelog object as in onShowDetails callback
-    },
-    onHideWidget: function(){
-      console.log("Who turned off the light?");
-    }
-  }
-}
+
 
 function App() {
+  const HW_config = {
+    selector: ".App-header", // CSS selector where to inject the badge
+    account:  "xGR4zy",
+    callbacks: {
+      onWidgetReady: function(widget) {
+        console.log("Widget is here!");
+        console.log("unseen entries count: " + widget.getUnseenCount());
+      },
+      onShowWidget: function(){
+        console.log("Someone opened the widget!");
+      },
+      onShowDetails: function(changelog){
+        console.log(changelog.position); // position in the widget
+        console.log(changelog.id); // unique id
+        console.log(changelog.title); // title
+        console.log(changelog.category); // category, lowercased
+      },
+      onReadMore: function(changelog){
+        console.log(changelog); // same changelog object as in onShowDetails callback
+      },
+      onHideWidget: function(){
+        console.log("Who turned off the light?");
+      }
+    }
+  }
 
   useEffect(() => {
     window.Headway.init(HW_config);
